d6a3e7ec053eeda295c481d1fb32112c2b65014c
mzbotz26 <mrsam2395@gmail.com>
2025-12-04 13:47:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("0"))
-API_HASH = os.getenv("")
-BOT_TOKEN = os.getenv("")
+API_ID = int(os.getenv("13441344"))
+API_HASH = os.getenv("2f10533d9068507d0c10bf1074527167")
+BOT_TOKEN = os.getenv("7771472205:AAENcUwaWSrT_zx2W8agFA_BS5auhOn7pN4")
 
-MONGO_URI = os.getenv("")
+MONGO_URI = os.getenv("mongodb+srv://Anant:Anant@movieforu.09ibn.mongodb.net/?retryWrites=true&w=majority&appName=Movieforu")
 DB_NAME = "forwarder_db"
 COLLECTION = "users"
