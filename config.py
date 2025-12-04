dd19cfd048cfcbc460948bb3c4ce2dc0be4315a0
mzbotz26 <mrsam2395@gmail.com>
2025-12-04 13:29:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv(""))
+API_HASH = os.getenv("")
+BOT_TOKEN = os.getenv("")
 
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("")
 DB_NAME = "forwarder_db"
 COLLECTION = "users"
