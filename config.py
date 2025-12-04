9a82510dbf344ac6bf948f41e8e239c949a65f8a
mzbotz26 <mrsam2395@gmail.com>
2025-12-04 15:42:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = "forwarder"
-COLLECTION = "links"
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Anant:Anant@movieforu.09ibn.mongodb.net/?retryWrites=true&w=majority&appName=Movieforu")   # IMPORTANT: set this in Heroku
+DB_NAME = os.getenv("DB_NAME", "forwarder")
+COLLECTION = os.getenv("COLLECTION", "links")
